6d94bc35d20114000ab5446b6bb147e5b2dbf1c3
👨🏽‍💻Dev <53455765+eniola07@users.noreply.github.com>
2025-05-18 20:02:25 +0100

    newnew

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -6,7 +6,7 @@ export const Users: CollectionConfig = {
     useAsTitle: 'email',
   },
   auth: {
-    verify: true, // ✅ email verification enabled here
+    verify: false, // ✅ email verification enabled here
   },
   access: {
     create: () => true, // ✅ correct location
